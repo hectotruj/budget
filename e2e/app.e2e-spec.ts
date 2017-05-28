e2d194768ae6df19f3f8ea5f861a573fb8b176be
Hector <hectoratiliotrujillo@gmail.com>
2017-05-28 14:17:55 -0400

    fixed e2e file merge conflicts

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { BudgetPage } from './app.po';
 
 describe('budget App', () => {
@@ -6,15 +6,6 @@ describe('budget App', () => {
 
   beforeEach(() => {
     page = new BudgetPage();
-=======
-import { MyAppPage } from './app.po';
-
-describe('my-app App', () => {
-  let page: MyAppPage;
-
-  beforeEach(() => {
-    page = new MyAppPage();
->>>>>>> 534b4e25668200509702d4b1a60b130263bb1569
   });
 
   it('should display message saying app works', () => {
